5e82f4967bd0befe71378226e3d947059d9154ae
Paul Spangler <spanglerco@users.noreply.github.com>
2020-07-24 08:46:43 -0500

    Bump version after release (#5)

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ author = "National Instruments"
 # The short X.Y version
 version = "0.1"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1"
 
 # --------------------------------------------------------------------------------------
 
