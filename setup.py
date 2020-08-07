2ae8bc01db7f144eb7c99579ae3b88816aa55aa5
Paul Spangler <paul.spangler@ni.com>
2020-08-07 14:02:04 -0500

    Bump version after release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def _get_version(name):
     script_dir = os.path.dirname(os.path.realpath(__file__))
     script_dir = os.path.join(script_dir, name)
     if not os.path.exists(os.path.join(script_dir, "VERSION")):
-        version = "0.1.1"
+        version = "0.1.2"
     else:
         with open(os.path.join(script_dir, "VERSION"), "r") as version_file:
             version = version_file.read().rstrip()
