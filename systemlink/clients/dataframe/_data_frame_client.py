9e94978afb9e4b8c39b992a2421df289b0ff2ead
Carson Moore <carson.moore@ni.com>
2022-11-09 23:23:12 -0600

    Add API info route

--- systemlink/clients/dataframe/_data_frame_client.py
+++ systemlink/clients/dataframe/_data_frame_client.py
@@ -1,7 +1,17 @@
+# TODO: Wrap Uplink decorators to add typing information
+# mypy: disable-error-code = misc
+
 """Implementation of DataFrameClient."""
 
+from typing import Any
+
 from systemlink.clients.core._uplink._base_client import BaseClient
+from uplink import get, returns
 
 
 class DataFrameClient(BaseClient):
-    pass
+    @returns.json()
+    @get("nidataframe")
+    def api_info(self) -> dict[str, Any]:
+        """Returns information about API versions and available operations."""
+        pass
