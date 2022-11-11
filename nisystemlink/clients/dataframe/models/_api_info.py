467e9ddd3266224165baf9d087b05ceea8f2ebf9
Carson Moore <carson.moore@ni.com>
2022-11-11 14:54:58 -0600

    Test attribute docs

--- nisystemlink/clients/dataframe/models/_api_info.py
+++ nisystemlink/clients/dataframe/models/_api_info.py
@@ -4,15 +4,15 @@ from nisystemlink.clients.core._uplink._json_model import JsonModel
 class Operation(JsonModel):
     """Represents an operation that can be performed on a data frame."""
 
-    available: bool
-    version: int
+    available: bool  #: Whether or not the operation is available to the caller (e.g. due to permissions).
+    version: int  #: The version of the available operation.
 
 
 class OperationsV1(JsonModel):
     """The operations available in the routes provided by the v1 HTTP API."""
 
-    create_tables: Operation
-    delete_tables: Operation
+    create_tables: Operation  #: The ability to create new DataFrame tables.
+    delete_tables: Operation  #: The ability to delete tables and all of their data.
     modify_metadata: Operation
     list_tables: Operation
     read_data: Operation
