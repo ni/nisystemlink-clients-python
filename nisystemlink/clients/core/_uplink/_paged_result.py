a526a0f7334c57f06dcecf3a8f080292d7633759
Carson <carson.moore@ni.com>
2022-11-18 10:18:32 -0600

    Fix whitespace in comment
    Co-authored-by: Paul Spangler <spanglerco@users.noreply.github.com>

--- nisystemlink/clients/core/_uplink/_paged_result.py
+++ nisystemlink/clients/core/_uplink/_paged_result.py
@@ -9,7 +9,7 @@ class PagedResult(JsonModel):
 
     When querying, a token will be returned if a query may be
     continued. To obtain the next page of results, pass the token to the service
-    on a subsequent request.The service will respond with a new continuation
+    on a subsequent request. The service will respond with a new continuation
     token. To paginate results, continue sending requests with the newest
     continuation token provided by the service, until this value is null.
     """
