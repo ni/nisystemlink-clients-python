1a3a4adb68ef6e93347422154bfd69e3c0b6b1a3
Santhosh Ramaraj <santhosh.ramaraj@solitontech.com>
2023-10-26 23:04:03 -0500

    Remove test for errors as it cannot be induced.

--- tests/integration/auth/test_auth.py
+++ tests/integration/auth/test_auth.py
@@ -1,5 +1,3 @@
-from copy import copy
-
 import pytest  # type: ignore
 from nisystemlink.clients.auth import AuthClient
 
@@ -10,14 +8,6 @@ def client(enterprise_config):
     return AuthClient(enterprise_config)
 
 
-@pytest.fixture(scope="class")
-def invalid_client(enterprise_config):
-    """Fixture to create a invalid AuthClient instance (invalid API Key)."""
-    invalid_enterprise_config = copy(enterprise_config)
-    invalid_enterprise_config.api_key = "abcdef1234"
-    return AuthClient(invalid_enterprise_config)
-
-
 @pytest.mark.enterprise
 @pytest.mark.integration
 class TestAuth:
@@ -25,8 +15,3 @@ class TestAuth:
         auth_response = client.get_auth()
         assert getattr(auth_response, "error") is None
         assert len(auth_response.workspaces) >= 1
-
-    def test__get_auth__errors(self, invalid_client: AuthClient):
-        auth_response = invalid_client.get_auth()
-        assert getattr(auth_response, "workspaces") is None
-        assert getattr(auth_response, "error") is not None
