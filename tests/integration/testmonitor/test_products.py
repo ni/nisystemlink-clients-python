bea9852cc2397adbf21246ada131021652906739
Adam Arnesen <adam.arnesen@ni.com>
2024-04-15 16:39:14 -0500

    add tests for API info route

---
+++ tests/integration/testmonitor/test_products.py
@@ -0,0 +1,17 @@
+import pytest
+from nisystemlink.clients.core._http_configuration import HttpConfiguration
+from nisystemlink.clients.testmonitor import TestMonitorClient
+
+
+@pytest.fixture(scope="class")
+def client(enterprise_config: HttpConfiguration) -> TestMonitorClient:
+    """Fixture to create a SpecClient instance."""
+    return TestMonitorClient(enterprise_config)
+
+
+@pytest.mark.integration
+@pytest.mark.enterprise
+class TestTestMonitor:
+    def test__api_info__returns(self, client: TestMonitorClient):
+        response = client.api_info()
+        assert len(response.dict()) != 0
