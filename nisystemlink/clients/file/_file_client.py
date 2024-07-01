7e68d9e734d31dfc23b2f1e75f329bb3bc097c0b
Santhosh Ramaraj <santhosh.ramaraj@solitontech.com>
2024-07-01 17:37:16 -0500

    feat: MVP for File Client to get, delete and download files

--- nisystemlink/clients/file/_file_client.py
+++ nisystemlink/clients/file/_file_client.py
@@ -1,6 +1,6 @@
 """Implementation of FileClient."""
 
-from typing import Dict, Optional
+from typing import Literal, Optional
 
 from nisystemlink.clients import core
 from nisystemlink.clients.core._uplink._base_client import BaseClient
@@ -12,7 +12,7 @@ from nisystemlink.clients.core._uplink._methods import (
 )
 from nisystemlink.clients.core.helpers import IteratorFileLike
 from requests.models import Response
-from uplink import Body, multipart, params, Part, Path, Query
+from uplink import Body, Path, Query
 
 from . import models
 
@@ -27,15 +27,15 @@ class FileClient(BaseClient):
 
         Args:
             configuration: Defines the web server to connect to and information about
-                how to connect. If not provided, an instance of
-                :class:`JupyterHttpConfiguration <nisystemlink.clients.core.JupyterHttpConfiguration>`
-                is used.
+                how to connect. If not provided, the
+                :class:`HttpConfigurationManager <nisystemlink.clients.core.HttpConfigurationManager>`
+                is used to obtain the configuration.
 
         Raises:
-            ApiException: if unable to communicate with the FileClient Service.
+            ApiException: if unable to communicate with the File Service.
         """
         if configuration is None:
-            configuration = core.JupyterHttpConfiguration()
+            configuration = core.HttpConfigurationManager.get_configuration()
 
         super().__init__(configuration, "/nifile/v1/")
 
@@ -47,69 +47,89 @@ class FileClient(BaseClient):
             Information about available API operations.
 
         Raises:
-            ApiException: if unable to communicate with the FileClient Service.
+            ApiException: if unable to communicate with the File Service.
         """
-        ...
-
-    @get("service-groups")
-    def get_service_groups(self):
-        """Get list of service groups.
-
-        Returns:
-            Information about available API operations.
-
-        Raises:
-            ApiException: if unable to communicate with the FileClient Service.
-        """
-        ...
 
     @get(
         "service-groups/Default/files",
         args=[
             Query,
             Query,
-            Query,
             Query(name="orderBy"),
             Query(name="orderByDescending"),
+            Query(name="id"),
         ],
     )
     def get_files(
         self,
-        id: str,
         skip: int = 0,
         take: int = 0,
-        order_by: models.OrderBy = models.OrderBy.CREATED,
-        order_by_descending: bool = False,
-    ):
+        order_by: Optional[
+            Literal["created", "id", "size", "lastUpdatedTimestamp"]
+        ] = None,
+        order_by_descending: Optional[Literal["true", "false"]] = "false",
+        file_ids: Optional[str] = None,
+    ) -> models.FileQueryResponse:
         """Lists available files on the SystemLink File service.
         Use the skip and take parameters to return paged responses.
         The orderBy and orderByDescending fields can be used to manage sorting the list by metadata objects.
-        """
-        ...
 
-    @delete("service-groups/Default/files/{id}", args=[Path, Query])
-    def delete_file(self, id: str, force: bool = False):
-        """Deletes the file indicated by the resource ID."""
-        ...
+        Args:
+            skip (int, optional): How many files to skip in the result when paging. Defaults to 0.
+            take (int, optional): How many files to return in the result, or 0 to use a default defined by the service.
+              Defaults to 0.
+            order_by (str, optional): The name of the metadata key to sort by. Defaults to None.
+            order_by_descending (str, optional): The elements in the list are sorted ascending if "false"
+              and descending if "true". Defaults to "false".
+            file_ids (Optional[str], optional): Comma-separated list of file IDs to search by. Defaults to None.
+
+        Returns:
+            models.FileQueryResponse: File Query Response
+
+        Raises:
+            ApiException: if unable to communicate with the File Service.
+        """
+
+    @delete("service-groups/Default/files/{file_id}", args=[Path, Query])
+    def delete_file(self, file_id: str, force: bool = False) -> None:
+        """Deletes the file indicated by the `file_id`.
+
+        Args:
+            file_id (str): The ID of the file.
+            force (bool, optional): Whether the deletion of a file will be forced. Defaults to False.
+
+        Raises:
+            ApiException: if unable to communicate with the File Service.
+        """
 
     @post("service-groups/Default/delete-files", args=[Body, Query])
-    def delete_files(self, files: models.DeleteMutipleRequest, force: bool = False):
-        ...
+    def delete_files(
+        self, files: models.DeleteMutipleRequest, force: bool = False
+    ) -> None:
+        """Delete multiple files.
 
-    @params({"inline": True})
+        Args:
+            files (models.DeleteMutipleRequest): The description of files to delete.
+            force (bool, optional): Whether the deletion of files will be forced. Defaults to False.
+
+        Raises:
+            ApiException: if unable to communicate with the File Service.
+        """
+
+    # TBD: Error with poe types - Untyped decorator makes function "download_file" untyped
+    # @params({"inline": True})
     @response_handler(_iter_content_filelike_wrapper)
-    @get("service-groups/Default/files/{id}/data")
-    def download_file(self, id: str) -> IteratorFileLike:
-        """Downloads a file from the SystemLink File service."""
-        ...
+    @get("service-groups/Default/files/{file_id}/data", args=[Path, Query])
+    def download_file(self, file_id: str, inline: bool = True) -> IteratorFileLike:
+        """Downloads a file from the SystemLink File service.
 
-    @multipart
-    @post(
-        "service-groups/Default/upload-files",
-        args=[Part, Part, Query(name="workspace"), Part],
-    )
-    def upload_file(
-        self, file: Part, metadata: Dict[str, str], workspace_id: str, id: str = None
-    ) -> models.UploadedFileInfo:
-        """Uploads a file using to the SystemLink File service."""
-        ...
+        Args:
+            file_id (str): The ID of the file.
+            inline (bool, optional): Return the file inline. Defaults to True.
+
+        Yields:
+            A file-like object for reading the exported data.
+
+        Raises:
+            ApiException: if unable to communicate with the File Service.
+        """
--- tests/integration/file/test_file_client.py
+++ tests/integration/file/test_file_client.py
@@ -1,8 +1,14 @@
-import io
+# from shutil import copyfileobj
+# from tempfile import TemporaryFile
+# from typing import Optional
 
 import pytest  # type: ignore
 from nisystemlink.clients.core import ApiException
 from nisystemlink.clients.file import FileClient
+from nisystemlink.clients.file.models import FileMetadata
+
+INVALID_FILE_ID = "Invalid-File-Id"
+FILE_NOT_FOUND_ERR = "Not Found"
 
 
 @pytest.fixture(scope="class")
@@ -11,10 +17,46 @@ def client(enterprise_config) -> FileClient:
     return FileClient(enterprise_config)
 
 
-@pytest.fixture(scope="class")
-def binary_file() -> io.BytesIO:
-    """Fixture to create a binary file as file pointer object."""
-    return io.BytesIO(b"123abc")
+# @pytest.fixture(scope="class")
+# def binary_file() -> io.BytesIO:
+#     """Fixture to create a binary file as file pointer object."""
+#     fake_file = io.BytesIO(b"123abc")
+#     fake_file.name = "Fake File.bin"
+#     return fake_file
+
+
+# @pytest.fixture(scope="class")
+# def upload_file(client: FileClient):
+#     """Fixture to return a factory that uploads a file."""
+#     file_ids = []
+
+#     def _upload_file(file) -> str:
+#         id = client.upload_file(file=file)
+#         file_ids.append(id)
+#         return id
+
+#     yield _upload_file
+
+#     _delete_files = DeleteMutipleRequest(ids=file_ids)
+#     client.delete_files(files=_delete_files, force=True)
+
+
+# @pytest.fixture(scope="class")
+# def test_file(upload_file, binary_file) -> str:
+#     """Fixture to create, upload a test file."""
+#     id = upload_file(binary_file)
+#     return id
+
+
+# @pytest.fixture(scope="class")
+# def test_download_file(client: FileClient) -> FileMetadata:
+#     """Get the MetaData of a File to download."""
+#     files = client.get_files(take=1)
+
+#     if files.available_files:
+#         return files.available_files[0]
+#     else:
+#         raise Exception("Require at least 1 File to download.")
 
 
 @pytest.mark.enterprise
@@ -24,11 +66,26 @@ class TestFileClient:
         api_info = client.api_info()
         assert len(api_info.dict()) != 0
 
-    def test__download_file__succeeds(self, client: FileClient):
-        client.download_file(id="45ab28ba-29a7-4d0e-9522-cc4c6cf100bf")
-        # with open(f"test_file.pdf", "wb") as f:
-        #     copyfileobj(data, f)
+    def test__get_files__succeeds(self, client: FileClient):
+        take_count = 1
+        files = client.get_files(take=take_count)
+
+        assert len(files.available_files) == take_count
+        assert files.total_count >= take_count
+
+    def test__delete_file__invalid_id_raises(self, client: FileClient):
+        with pytest.raises(ApiException, match=FILE_NOT_FOUND_ERR):
+            client.delete_file(file_id=INVALID_FILE_ID, force=True)
+
+    # def test__delete_file__succeeds(self, client: FileClient):
+    #     client.delete_file(file_id="7729547d-95af-4beb-a7a8-a913c0a23de4", force=True)
 
     def test__download_file__invalid_id_raises(self, client: FileClient):
-        with pytest.raises(ApiException, match="Not Found"):
-            client.download_file(id="Invalid ID")
+        with pytest.raises(ApiException, match=FILE_NOT_FOUND_ERR):
+            client.download_file(file_id=INVALID_FILE_ID)
+
+    # def test__download_file__succeeds(self, client: FileClient, test_download_file: FileMetadata):
+    #     file_id = test_download_file.id
+    #     data = client.download_file(file_id=file_id)
+    #     with open(f"downloaded_file", "wb") as f:
+    #         copyfileobj(data, f)
