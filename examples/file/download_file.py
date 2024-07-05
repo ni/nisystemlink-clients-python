d06eb7b7fda072d9b96385e4d0f2b61d79e627f6
Santhosh Ramaraj <santhosh.ramaraj@solitontech.com>
2024-07-05 12:32:03 -0500

    feat: Use file name from metadata

--- examples/file/download_file.py
+++ examples/file/download_file.py
@@ -8,7 +8,13 @@ client = FileClient()
 
 file_id = "<Id of file to download>"
 
-file_name = f"{file_id}.extension"
+# Fetch the file metadata to get the name
+files = client.get_files(file_ids=[file_id])
+
+if not files.available_files:
+    raise Exception(f"File ID {file_id} not found.")
+
+file_name = files.available_files[0].properties.get("Name")
 
 # Download the file using FileId with content inline
 content = client.download_file(file_id=file_id)
