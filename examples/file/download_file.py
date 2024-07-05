714f3a6df61fc3944823dd4913f903aa4a17af41
Santhosh Ramaraj <santhosh.ramaraj@solitontech.com>
2024-07-05 12:33:48 -0500

    feat: Use untitled as filename if Name is not available

--- examples/file/download_file.py
+++ examples/file/download_file.py
@@ -14,7 +14,7 @@ files = client.get_files(file_ids=[file_id])
 if not files.available_files:
     raise Exception(f"File ID {file_id} not found.")
 
-file_name = files.available_files[0].properties.get("Name")
+file_name = files.available_files[0].properties.get("Name", "Untitled")
 
 # Download the file using FileId with content inline
 content = client.download_file(file_id=file_id)
