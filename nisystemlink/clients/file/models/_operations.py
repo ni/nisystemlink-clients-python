922742610c1e2bc13463b230275f57b4ce4ad016
Santhosh Ramaraj <santhosh.ramaraj@solitontech.com>
2024-09-16 14:29:15 -0500

    refactor: reuse existing `Operation` model

--- nisystemlink/clients/file/models/_operations.py
+++ nisystemlink/clients/file/models/_operations.py
@@ -1,32 +1,20 @@
 from typing import Optional
 
+from nisystemlink.clients.core._api_info import Operation
 from nisystemlink.clients.core._uplink._json_model import JsonModel
-from pydantic import Field
-
-
-class Operation5(JsonModel):
-    available: bool
-    """
-    Whether the operation is available to the caller
-    """
-
-    version: Optional[int] = None
-    """
-    The version of the available operation
-    """
 
 
 class Operations(JsonModel):
-    delete_files: Optional[Operation5] = Field(None, alias="deleteFiles")
-    download_data: Optional[Operation5] = Field(None, alias="downloadData")
-    list_files: Optional[Operation5] = Field(None, alias="listFiles")
-    query_files: Optional[Operation5] = Field(None, alias="queryFiles")
-    update_metadata: Optional[Operation5] = Field(None, alias="updateMetadata")
-    upload_files: Optional[Operation5] = Field(None, alias="uploadFiles")
+    delete_files: Optional[Operation]
+    download_data: Optional[Operation]
+    list_files: Optional[Operation]
+    query_files: Optional[Operation]
+    update_metadata: Optional[Operation]
+    upload_files: Optional[Operation]
 
 
 class V1Operations(JsonModel):
-    operations: Optional[Operations] = None
+    operations: Optional[Operations]
     """
     Available operations in the v1 version of the API:
     - deleteFiles: The ability to delete uploaded files
