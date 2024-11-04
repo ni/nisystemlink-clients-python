d362dd783006d6011df6a5a257d7ef12a5994fa0
Adam Arnesen <adam.arnesen@ni.com>
2024-11-04 15:32:16 -0600

    rename file to match class name

--- nisystemlink/clients/testmonitor/models/__init__.py
+++ nisystemlink/clients/testmonitor/models/__init__.py
@@ -1,6 +1,6 @@
 from ._api_info import Operation, V2Operations, ApiInfo
 from ._product import Product
-from ._create_products_request import CreateProductsPartialSuccess
+from ._create_products_partial_success import CreateProductsPartialSuccess
 from ._delete_products_partial_success import DeleteProductsPartialSuccess
 from ._paged_products import PagedProducts
 from ._query_products_request import (
