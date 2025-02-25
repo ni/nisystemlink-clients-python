a70692aac0ca506732e9e4bf645234d8614fe977
Priyadarshini Piramanayagam <priydarshini.piramanayagam@emerson.com>
2025-02-25 23:39:20 +0530

    remove arg for update-results

--- nisystemlink/clients/result/_result_client.py
+++ nisystemlink/clients/result/_result_client.py
@@ -126,10 +126,7 @@ class ResultClient(BaseClient):
 
     @post("update-results", args=[Field("results"), Field("replace")])
     def update_results(
-        self,
-        results: List[Result],
-        replace: bool = False,
-        allow_workspace_update: bool = False,
+        self, results: List[Result], replace: bool = False
     ) -> models.ResultsPartialSuccess:
         """Updates a list of results with optional field replacement.
 
@@ -148,10 +145,7 @@ class ResultClient(BaseClient):
             ApiException: if unable to communicate with the ``/nitestmonitor`` Service
                 or provided an invalid argument.
         """
-        if not allow_workspace_update:
-            for result in results:
-                result.workspace = None
-        return self._update_results(results, replace)
+        ...
 
     @delete("results/{id}")
     def delete_result(self, id: str) -> None:
