e9feff6dc3e473dd34cbb53739f1c96eb7a8db0e
Shriram S <141298898+shri2k2@users.noreply.github.com>
2025-03-05 04:10:56 +0530

    feat: Add and resolve query projection for products and specs respectively (#97)
    Co-authored-by: Sam rishi <sam.rishi@solitontech.local>
    Co-authored-by: sam-rishi <155733081+sam-rishi@users.noreply.github.com>
    Co-authored-by: Sam Rishi <sam.rishi@ni.com>
    
    BREAKING CHANGE: Product and Specification models have been updated
    to support projections.
     - Product client changes--
       - `models.Product` now defines all fields as Optional.
       - `ProductClient.create_product`'s `products` parameter is now typed
         as `models.CreateProductRequest`
       - `ProductClient.update_product`'s `products` parameter is now typed
         as `models.UpdateProductRequest`
     - Specifications client changes--
       - `models.Specification` and `models.SpecificationDefinition` now
         define all fields as Optional.
       - `models.QuerySpecifications` has been renamed `models.PagedSpecifications`
         to better align to other clients.
       - `models.CreateSpecificationeRequest.specs` is now typed as `models.CreateSpecificationsRequestObject`
         instead of `models.SpecificationDefinition`
       - `models.UpdateSpecificationeRequest.specs` is now typed as `models.UpdateSpecificationsRequestObject`
         instead of `models.Specification`

--- nisystemlink/clients/product/models/__init__.py
+++ nisystemlink/clients/product/models/__init__.py
@@ -1,11 +1,14 @@
-from ._product import Product
 from ._create_products_partial_success import CreateProductsPartialSuccess
 from ._delete_products_partial_success import DeleteProductsPartialSuccess
 from ._paged_products import PagedProducts
 from ._query_products_request import (
-    QueryProductsRequest,
     ProductField,
+    ProductOrderBy,
+    ProductProjection,
+    QueryProductsRequest,
     QueryProductValuesRequest,
 )
+from ._product import Product
+from ._product_request import CreateProductRequest, UpdateProductRequest
 
 # flake8: noqa
--- nisystemlink/clients/spec/models/__init__.py
+++ nisystemlink/clients/spec/models/__init__.py
@@ -10,24 +10,25 @@ from ._create_specs_request import (
     CreatedSpecification,
     CreateSpecificationsPartialSuccess,
     CreateSpecificationsRequest,
+    CreateSpecificationsRequestObject,
 )
 from ._delete_specs_request import DeleteSpecificationsPartialSuccess
-from ._query_specs import QuerySpecificationsRequest, QuerySpecifications
+from ._query_specs import (
+    QuerySpecificationsRequest,
+    PagedSpecifications,
+    SpecificationProjection,
+)
 from ._specification import (
     Specification,
-    SpecificationCreation,
     SpecificationDefinition,
     SpecificationLimit,
-    SpecificationServerManaged,
     SpecificationType,
-    SpecificationUpdated,
-    SpecificationUserManaged,
-    SpecificationWithHistory,
 )
 from ._update_specs_request import (
     UpdatedSpecification,
     UpdateSpecificationsPartialSuccess,
     UpdateSpecificationsRequest,
+    UpdateSpecificationsRequestObject,
 )
 
 # flake8: noqa
