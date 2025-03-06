1c5d889fa97c653f18fda7137da4a6a49531661d
ShriramS-Emerson <shriram.sakthivel@emerson.com>
2025-03-06 20:13:21 +0530

    update doc string

--- nisystemlink/clients/product/utilities/_dataframe_utilities.py
+++ nisystemlink/clients/product/utilities/_dataframe_utilities.py
@@ -12,7 +12,13 @@ def convert_products_to_dataframe(products: List[Product]) -> DataFrame:
         products (List[Product]): A list of product responses retrieved from the API.
 
     Returns:
-        DataFrame: A Pandas DataFrame containing the normalized product data.
+        DataFrame:
+            - A Pandas DataFrame containing the normalized product data. The DataFrame would consist
+            of all the fields received from the product response.
+
+            - Further, the properties field alone would get normalized. In the sense, there
+            would be separate columns for each of the properties in the list of all products. The
+            normalized column headers would be in the format `properties.property_name`.
     """
     products_dict_representation = [product.dict() for product in products]
     normalized_products_dataframe = pd.json_normalize(
