0b4a6fbee5dfc8ea205fe4a7f69f069d00df24c2
ShriramS-Emerson <shriram.sakthivel@emerson.com>
2025-03-07 20:06:02 +0530

    fix: resolve PR comments

--- nisystemlink/clients/testmonitor/utilities/_dataframe_utilities.py
+++ nisystemlink/clients/testmonitor/utilities/_dataframe_utilities.py
@@ -11,94 +11,122 @@ from pandas import DataFrame
 def convert_steps_to_dataframe(steps: List[Step]) -> DataFrame:
     """Converts a list of steps into a normalized dataframe
 
+    - A new column would be created for unique `properties` across all steps. The property
+    columns would be named in the format `properties.property_name`.
+    - `Inputs` and `Outputs` are converted from a list of name-value pairs to a dict and then
+    normalized - similar to properties.
+    - For each `parameter` entry in `data`, a new row is added in the dataframe, with all the
+    other values are duplicated.
+
     Args:
-        steps (List[Step]): A list of steps.
+        steps: A list of steps.
 
     Returns:
-        DataFrame: A Pandas DataFrame containing the normalized steps data.
+        DataFrame:
+            - A Pandas DataFrame containing the steps data. The DataFrame would consist of all the
+            fields in the input steps.
+            - A new column would be created for unique `properties` across all steps. The property
+            columns would be named in the format `properties.property_name`.
+            - `Inputs` and `Outputs` are converted from a list of name-value pairs to a dict and then
+            normalized - similar to properties.
+            - For each `parameter` entry in `data`, a new row is added in the dataframe, with all the
+            other values are duplicated.
     """
-    steps_dict_representation = [step.dict(exclude_unset=True) for step in steps]
-    restructured_steps = __restructure_steps(steps=steps_dict_representation)
+    DATA_PARAMETERS = "data.parameters"
 
-    normalized_steps_dataframe = pd.json_normalize(restructured_steps, sep=".")
-    grouped_columns = __group_step_columns(normalized_steps_dataframe.columns)
-    normalized_steps_dataframe = normalized_steps_dataframe.reindex(
-        columns=grouped_columns
-    )
-    normalized_steps_dataframe.dropna(axis="columns", how="all", inplace=True)
+    restructured_steps = __restructure_steps(steps)
 
-    return normalized_steps_dataframe
+    # checking if `data` exists in the steps. the following logic is specific to process `data` field
+    if steps and steps[0].data:
+        steps_dataframe = pd.json_normalize(restructured_steps, sep=".").explode(
+            DATA_PARAMETERS, ignore_index=True
+        )
+        steps_dataframe = pd.concat(
+            [
+                steps_dataframe.drop(columns=[DATA_PARAMETERS]),
+                pd.json_normalize(steps_dataframe[DATA_PARAMETERS]).add_prefix(
+                    f"{DATA_PARAMETERS}."
+                ),
+            ],
+            axis=1,
+        )
+    else:
+        steps_dataframe = pd.json_normalize(restructured_steps, sep=".")
+
+    grouped_columns = __group_step_columns(steps_dataframe.columns)
+
+    return steps_dataframe.reindex(columns=grouped_columns)
 
 
-def __restructure_steps(steps: List[Dict[str, Any]]) -> List:
-    """Restructures the list of inputs and outputs into dictionaries, with the key as the input or
-    output's name and value as the actual value of the input or output respectively.
+def __restructure_steps(steps: List[Step]) -> List[Dict[str, Any]]:
+    """Restructures a list of step responses by converting input and output lists into dictionaries.
 
-    This will be helpful when normalizing the steps into a dataframe. If not, then the inputs and outputs
-    will be defined as an array of values, within a single cell in the dataframe respectively.
+    Each dictionary maps input/output names to their corresponding values, making it easier to
+    normalize the data into a DataFrame. Without this transformation, inputs and outputs would
+    remain as lists within a single cell.
 
     Args:
-        steps (List): A list of step responses retrieved from the API.
+        steps: A list of step responses retrieved from the API.
 
     Returns:
         List[Step]: Restructured steps - modification involves the conversion of list of inputs and outputs
         into dictionaries respectively.
     """
-
-    def process_io(io_list: List) -> Dict:
-        return {io["name"]: io["value"] for io in io_list}
-
-    input_key = StepProjection.INPUTS.lower()
-    output_key = StepProjection.OUTPUTS.lower()
+    restructured_steps = []
 
     for step in steps:
-        if input_key in step and step[input_key]:
-            step[input_key] = process_io(step.get(input_key, []))
-        if output_key in step and step[output_key]:
-            step[output_key] = process_io(step.get(output_key, []))
+        step_dict = step.dict(exclude_none=True)
+        step_dict[StepProjection.INPUTS.lower()] = (
+            {item.name: item.value for item in step.inputs} if step.inputs else {}
+        )
+        step_dict[StepProjection.OUTPUTS.lower()] = (
+            {item.name: item.value for item in step.outputs} if step.outputs else {}
+        )
 
-    return steps
+        restructured_steps.append(step_dict)
+
+    return restructured_steps
 
 
 def __group_step_columns(df_columns: List[str]) -> List[str]:
-    """Gets the list of columns in the dataframe, group them under various categories and orders them.
+    """Groups and orders dataframe columns into predefined categories to maintain a consistent structure.
 
-    When normalizing the steps into a dataframe, there would be inconsistency in the ordering of the
-    columns. For example, consider from a list of steps the first step has been added as a column.
-    Now when including the next step, if there are new input, output or property fields, they would get
-    added as the last column in the dataframe, which would break the actual expected column flow in the
-    dataframe. Using this function, the columns can be grouped together.
+    When normalizing steps into a dataframe, new input, output, or property fields may be added at the end,
+    disrupting the expected column order. This function ensures columns are grouped properly.
 
     Args:
-        df_columns (List[str]): The list of all columns from the normalized dataframe.
+        df_columns: The list of all columns from the normalized dataframe.
 
     Returns:
         List[str]: A list containing grouped and ordered columns.
     """
-    grouped_columns: Dict[str, List[str]] = {
-        "general": [],
-        StepProjection.INPUTS: [],
-        StepProjection.OUTPUTS: [],
-        StepProjection.DATA: [],
-        StepProjection.PROPERTIES: [],
-    }
+    GENERAL_CATEGORIES = "general"
+    CATEGORY_KEYS = [
+        GENERAL_CATEGORIES,
+        StepProjection.INPUTS,
+        StepProjection.OUTPUTS,
+        StepProjection.DATA,
+        StepProjection.PROPERTIES,
+    ]
+
+    grouped_columns: Dict[str, List[str]] = {category: [] for category in CATEGORY_KEYS}
 
     for column in df_columns:
-        if StepProjection.DATA.lower() in column and not column == "data_model":
+        column_lower = column.lower()
+        if (
+            StepProjection.DATA.lower() in column_lower
+            and column != StepProjection.DATA_MODEL.lower()
+        ):
             grouped_columns[StepProjection.DATA].append(column)
-        elif StepProjection.INPUTS.lower() in column:
+        elif StepProjection.INPUTS.lower() in column_lower:
             grouped_columns[StepProjection.INPUTS].append(column)
-        elif StepProjection.OUTPUTS.lower() in column:
+        elif StepProjection.OUTPUTS.lower() in column_lower:
             grouped_columns[StepProjection.OUTPUTS].append(column)
-        elif StepProjection.PROPERTIES.lower() in column:
+        elif StepProjection.PROPERTIES.lower() in column_lower:
             grouped_columns[StepProjection.PROPERTIES].append(column)
         else:
-            grouped_columns["general"].append(column)
+            grouped_columns[GENERAL_CATEGORIES].append(column)
 
-    return (
-        grouped_columns["general"]
-        + grouped_columns[StepProjection.INPUTS]
-        + grouped_columns[StepProjection.OUTPUTS]
-        + grouped_columns[StepProjection.DATA]
-        + grouped_columns[StepProjection.PROPERTIES]
-    )
+    return [
+        column for category in CATEGORY_KEYS for column in grouped_columns[category]
+    ]
---
+++ tests/testmonitor/test_testmonitor_dataframe_utilities.py
@@ -0,0 +1,289 @@
+from datetime import datetime, timezone
+from typing import List
+
+import pandas as pd
+import pytest
+from nisystemlink.clients.testmonitor.models import (
+    NamedValue,
+    Status,
+    StatusType,
+    Step,
+    StepData,
+)
+from nisystemlink.clients.testmonitor.utilities import convert_steps_to_dataframe
+from pandas import DataFrame
+
+
+@pytest.fixture
+def mock_steps_data() -> List[Step]:
+    """Fixture to return a mock step data."""
+    step1 = Step(
+        name="MockStep1",
+        step_type="StepType",
+        step_id="5ffb2bf6771fa11e877838dd1",
+        parent_id="5ffb2bf6771fa11e877838dd2",
+        result_id="5ffb2bf6771fa11e877838dd3",
+        path="Path",
+        path_ids=["path_id_11", "path_id_12"],
+        status=Status(status_type=StatusType.DONE, status_name="Done"),
+        total_time_in_seconds=5,
+        started_at=datetime(2023, 3, 27, 18, 39, 49, tzinfo=timezone.utc),
+        updated_at=datetime(2024, 2, 2, 14, 22, 4, 625155, tzinfo=timezone.utc),
+        inputs=[
+            NamedValue(name="Input11", value="input_value_11"),
+            NamedValue(name="Input12", value="input_value_12"),
+        ],
+        outputs=[
+            NamedValue(name="Output11", value="output_value_11"),
+            NamedValue(name="Output12", value="output_value_12"),
+        ],
+        data_model="STDF",
+        data=StepData(
+            text="data1",
+            parameters=[
+                {
+                    "name": "parameter_11",
+                    "units": "A",
+                    "status": "Failed",
+                    "lowLimit": "6.0",
+                    "highLimit": "21.0",
+                    "measurement": "11.0",
+                    "comparisonType": "GTLT",
+                },
+                {
+                    "name": "parameter_12",
+                    "units": "B",
+                    "status": "Passed",
+                    "lowLimit": "7.0",
+                    "highLimit": "22.0",
+                    "measurement": "12.0",
+                    "comparisonType": "GTLT",
+                },
+            ],
+        ),
+        has_children=False,
+        workspace="846e294a-a007-47ac-9fc2-fac07eab240f",
+        keywords=["keyword11", "keyword12"],
+        properties={"property11": "property11_value", "property12": "property12_value"},
+    )
+    step2 = Step(
+        name="MockStep2",
+        step_type="StepType",
+        step_id="5ffb2bf6771fa11e877838dd6",
+        parent_id="5ffb2bf6771fa11e877838dd7",
+        result_id="5ffb2bf6771fa11e877838dd8",
+        path="Path",
+        path_ids=["path_id_21", "path_id_22"],
+        status=Status(status_type=StatusType.DONE, status_name="Done"),
+        total_time_in_seconds=5,
+        started_at=datetime(2023, 3, 27, 18, 39, 49, tzinfo=timezone.utc),
+        updated_at=datetime(2024, 2, 2, 14, 22, 4, 625255, tzinfo=timezone.utc),
+        inputs=[
+            NamedValue(name="Input21", value="input_value_21"),
+            NamedValue(name="Input22", value="input_value_22"),
+        ],
+        outputs=[
+            NamedValue(name="Output21", value="output_value_21"),
+            NamedValue(name="Output22", value="output_value_22"),
+        ],
+        data_model="STDF",
+        data=StepData(
+            text="data2",
+            parameters=[
+                {
+                    "name": "parameter_21",
+                    "units": "A",
+                    "status": "Passed",
+                    "lowLimit": "6.0",
+                    "highLimit": "21.0",
+                    "measurement": "11.0",
+                    "comparisonType": "GTLT",
+                },
+                {
+                    "name": "parameter_22",
+                    "units": "C",
+                    "status": "Passed",
+                    "lowLimit": "7.0",
+                    "highLimit": "22.0",
+                    "measurement": "12.0",
+                    "comparisonType": "GTLT",
+                },
+            ],
+        ),
+        has_children=True,
+        workspace="846e294a-a007-47ac-9fc2-fac07eab240z",
+        keywords=["keyword21", "keyword22"],
+        properties={"property21": "property21_value", "property22": "property22_value"},
+    )
+
+    return [step1, step2]
+
+
+@pytest.fixture
+def expected_steps_dataframe(mock_steps_data: List[Step]) -> DataFrame:
+    """Fixture to return the expected DataFrame based on the mock step data."""
+    restructured_mock_steps = []
+
+    for step in mock_steps_data:
+        properties = (
+            {f"properties.{key}": value for key, value in step.properties.items()}
+            if step.properties
+            else {}
+        )
+        inputs = (
+            {f"inputs.{item.name}": item.value for item in step.inputs}
+            if step.inputs
+            else {}
+        )
+        outputs = (
+            {f"outputs.{item.name}": item.value for item in step.outputs}
+            if step.outputs
+            else {}
+        )
+
+        for parametric_data in step.data.parameters:
+            parameter_data = {
+                f"data.parameters.{key}": value
+                for key, value in parametric_data.items()
+            }
+            restructured_step = {
+                "name": step.name,
+                "step_type": step.step_type,
+                "step_id": step.step_id,
+                "parent_id": step.parent_id,
+                "result_id": step.result_id,
+                "path": step.path,
+                "path_ids": step.path_ids,
+                "total_time_in_seconds": step.total_time_in_seconds,
+                "started_at": step.started_at,
+                "updated_at": step.updated_at,
+                "data_model": step.data_model,
+                "has_children": step.has_children,
+                "workspace": step.workspace,
+                "keywords": step.keywords,
+                "status.status_type": step.status.status_type,
+                "status.status_name": step.status.status_name,
+                **inputs,
+                **outputs,
+                "data.text": step.data.text,
+                **parameter_data,
+                **properties,
+            }
+            restructured_mock_steps.append(restructured_step)
+
+    expected_dataframe = DataFrame(restructured_mock_steps)
+    expected_column_order = [
+        "name",
+        "step_type",
+        "step_id",
+        "parent_id",
+        "result_id",
+        "path",
+        "path_ids",
+        "total_time_in_seconds",
+        "started_at",
+        "updated_at",
+        "data_model",
+        "has_children",
+        "workspace",
+        "keywords",
+        "status.status_type",
+        "status.status_name",
+        "inputs.Input11",
+        "inputs.Input12",
+        "inputs.Input21",
+        "inputs.Input22",
+        "outputs.Output11",
+        "outputs.Output12",
+        "outputs.Output21",
+        "outputs.Output22",
+        "data.text",
+        "data.parameters.name",
+        "data.parameters.units",
+        "data.parameters.status",
+        "data.parameters.lowLimit",
+        "data.parameters.highLimit",
+        "data.parameters.measurement",
+        "data.parameters.comparisonType",
+        "properties.property11",
+        "properties.property12",
+        "properties.property21",
+        "properties.property22",
+    ]
+
+    return expected_dataframe.reindex(columns=expected_column_order)
+
+
+@pytest.fixture
+def empty_steps_data() -> List:
+    """Fixture to return an empty list of steps."""
+    return []
+
+
+@pytest.mark.enterprise
+@pytest.mark.unit
+class TestTestMonitorDataframeUtilities:
+    def test__convert_steps_to_dataframe__with_complete_data(
+        self, mock_steps_data: List[Step], expected_steps_dataframe: DataFrame
+    ):
+        """Test normal case with valid step data."""
+        steps_dataframe = convert_steps_to_dataframe(mock_steps_data)
+
+        assert not steps_dataframe.empty
+        assert (
+            steps_dataframe.columns.to_list()
+            == expected_steps_dataframe.columns.to_list()
+        )
+        assert steps_dataframe["updated_at"].dtype == "datetime64[ns, UTC]"
+        assert steps_dataframe["started_at"].dtype == "datetime64[ns, UTC]"
+        assert steps_dataframe["path_ids"].dtype == "object"
+        assert isinstance(steps_dataframe["path_ids"].iloc[0], List)
+        assert steps_dataframe["keywords"].dtype == "object"
+        assert isinstance(steps_dataframe["keywords"].iloc[0], List)
+        pd.testing.assert_frame_equal(
+            steps_dataframe, expected_steps_dataframe, check_dtype=True
+        )
+
+    def test__convert_steps_to_dataframe__with_empty_data(self, empty_steps_data: List):
+        """Test case when the input steps data is empty."""
+        steps_dataframe = convert_steps_to_dataframe(empty_steps_data)
+
+        print(steps_dataframe)
+
+        assert steps_dataframe.empty
+
+    def test__convert_steps_to_dataframe__with_missing_fields(
+        self, mock_steps_data: List[Step], expected_steps_dataframe: DataFrame
+    ):
+        """Test case when some fields in step data are missing."""
+        steps = mock_steps_data
+        for step in steps:
+            step.path_ids = None
+            step.data = None
+
+        steps_dataframe = convert_steps_to_dataframe(steps)
+        expected_steps_dataframe = expected_steps_dataframe.drop(
+            columns=[
+                "path_ids",
+                "data.parameters.name",
+                "data.parameters.units",
+                "data.parameters.status",
+                "data.parameters.lowLimit",
+                "data.parameters.highLimit",
+                "data.parameters.measurement",
+                "data.parameters.comparisonType",
+                "data.text",
+            ]
+        )
+        expected_steps_dataframe = expected_steps_dataframe.drop_duplicates(
+            subset=["step_id", "result_id"], ignore_index=True
+        )
+
+        assert not steps_dataframe.empty
+        assert (
+            steps_dataframe.columns.to_list()
+            == expected_steps_dataframe.columns.to_list()
+        )
+        pd.testing.assert_frame_equal(
+            steps_dataframe, expected_steps_dataframe, check_dtype=True
+        )
