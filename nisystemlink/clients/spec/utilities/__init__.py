f89ae64aad3623b06ac2a887669f341af2ec91b6
Sam rishi <sam.rishi@solitontech.local>
2025-03-11 18:24:21 +0530

    refactor:ExposeConditionFormatMethod

--- nisystemlink/clients/spec/utilities/__init__.py
+++ nisystemlink/clients/spec/utilities/__init__.py
@@ -1,3 +1,6 @@
-from ._dataframe_utilities import convert_specs_to_dataframe
+from ._dataframe_utilities import (
+    convert_specs_to_dataframe,
+    summarize_conditions_as_a_string,
+)
 
 # flake8: noqa
