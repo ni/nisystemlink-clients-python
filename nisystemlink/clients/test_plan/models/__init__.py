2231a4a16341ea81f07401f9227c8a1c4b5203be
Aravindhan Palanisamy <aravindhan.palanisamy@ni.corp.natinst.com>
2025-05-23 11:14:05 +0530

    refactor file names

--- nisystemlink/clients/test_plan/models/__init__.py
+++ nisystemlink/clients/test_plan/models/__init__.py
@@ -1,66 +1,66 @@
-from nisystemlink.clients.test_plan.models._create_test_plan_request import (
+from ._create_test_plan_request import (
     CreateTestPlanRequest,
 )
-from nisystemlink.clients.test_plan.models._create_test_plans_request import (
+from ._create_test_plans_request import (
     CreateTestPlansRequest,
 )
-from nisystemlink.clients.test_plan.models._create_test_plans_response import (
+from ._create_test_plans_response import (
     CreateTestPlansResponse,
 )
-from nisystemlink.clients.test_plan.models._delete_test_plans_request import (
+from ._delete_test_plans_request import (
     DeleteTestPlansRequest,
 )
-from nisystemlink.clients.test_plan.models._update_test_plan_request import (
+from ._update_test_plan_request import (
     UpdateTestPlanRequest,
 )
-from nisystemlink.clients.test_plan.models._update_test_plans_request import (
+from ._update_test_plans_request import (
     UpdateTestPlansRequest,
 )
-from nisystemlink.clients.test_plan.models._update_test_plans_response import (
+from ._update_test_plans_response import (
     UpdateTestPlansResponse,
 )
-from nisystemlink.clients.test_plan.models._query_test_plans_request import (
+from ._query_test_plans_request import (
     QueryTestPlansRequest,
     _QueryTestPlansRequest,
 )
-from nisystemlink.clients.test_plan.models._paged_test_plans import PagedTestPlans
-from nisystemlink.clients.test_plan.models._execution_event import ExecutionEvent
-from nisystemlink.clients.test_plan.models._order_by import OrderBy
-from nisystemlink.clients.test_plan.models._test_plan import TestPlan
-from nisystemlink.clients.test_plan.models._state import State
-from nisystemlink.clients.test_plan.models._execution_definition import (
+from ._paged_test_plans import PagedTestPlans
+from ._execution_event import ExecutionEvent
+from ._order_by import OrderBy
+from ._test_plan import TestPlan
+from ._state import State
+from ._execution_definition import (
     ExecutionDefinition,
 )
-from nisystemlink.clients.test_plan.models._schedule_test_plans_request import (
+from ._schedule_test_plans_request import (
     ScheduleTestPlansRequest,
 )
-from nisystemlink.clients.test_plan.models._schedule_test_plan_request import (
+from ._schedule_test_plan_request import (
     ScheduleTestPlanRequest,
 )
-from nisystemlink.clients.test_plan.models._schedule_test_plans_response import (
+from ._schedule_test_plans_response import (
     ScheduleTestPlansResponse,
 )
-from nisystemlink.clients.test_plan.models._workflow_definition import (
+from ._workflow_definition import (
     WorkflowDefinition,
 )
-from nisystemlink.clients.test_plan.models._test_plan_templates import (
+from ._test_plan_templates import (
     TestPlanTemplateBase,
     TestPlanTemplate,
 )
-from nisystemlink.clients.test_plan.models._create_test_plan_templates_partial_success_response import (
+from ._create_test_plan_templates_partial_success_response import (
     CreateTestPlanTemplatePartialSuccessResponse,
 )
-from nisystemlink.clients.test_plan.models._delete_test_plan_templates_partial_success_response import (
+from ._delete_test_plan_templates_partial_success_response import (
     DeleteTestPlanTemplatesPartialSuccessResponse,
 )
-from nisystemlink.clients.test_plan.models._query_test_plan_templates_request import (
+from ._query_test_plan_templates_request import (
     QueryTestPlanTemplatesRequest,
     _QueryTestPlanTemplatesRequest,
 )
-from nisystemlink.clients.test_plan.models._paged_test_plan_templates import (
+from ._paged_test_plan_templates import (
     PagedTestPlanTemplates,
 )
-from nisystemlink.clients.test_plan.models._create_test_plan_template_request import (
+from ._create_test_plan_template_request import (
     CreateTestPlanTemplateRequest,
 )
 
