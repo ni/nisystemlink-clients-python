681b0f418e100690ccc7e9f2b5484c20a6cc9f64
Paul Spangler <7519484+spanglerco@users.noreply.github.com>
2025-10-20 12:10:04 -0500

    Add new APIs to models

--- nisystemlink/clients/dataframe/models/_create_table_request.py
+++ nisystemlink/clients/dataframe/models/_create_table_request.py
@@ -18,5 +18,11 @@ class CreateTableRequest(JsonModel):
     properties: Optional[Dict[str, str]] = None
     """User-defined properties to associate with the table."""
 
+    test_result_id: Optional[str] = None
+    """The ID of the test result associated with the table. A value of ``None`` or an empty string
+    indicates there is no associated test result. Added in version 2 of the
+    :py:attr:`nisystemlink.clients.dataframe.models.OperationsV1.create_tables` operation. Older
+    versions of the service will ignore this value."""
+
     workspace: Optional[str] = None
     """The workspace to create the table in. Uses the default workspace when not specified."""
