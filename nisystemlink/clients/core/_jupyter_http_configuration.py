6c3e6bef321050de5ef39ae37ecd31f5df2ae4e0
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2026-07-15 22:46:54 +0300

    Fix linting for _jupyter_http_configuration.py

--- nisystemlink/clients/core/_jupyter_http_configuration.py
+++ nisystemlink/clients/core/_jupyter_http_configuration.py
@@ -13,7 +13,10 @@ class JupyterHttpConfiguration(core.HttpConfiguration):
 
     _HTTP_URI_ENV_VAR = "SYSTEMLINK_HTTP_URI"
     _HTTP_API_KEY_ENV_VAR = "SYSTEMLINK_API_KEY"
-    _SYSTEMLINK_SERVER_CERT_PATH = r"C:\ProgramData\National Instruments\Skyline\Certificates\http-server\http-server.cer"
+    _SYSTEMLINK_SERVER_CERT_PATH = (
+        r"C:\ProgramData\National Instruments\Skyline"
+        r"\Certificates\http-server\http-server.cer"
+    )
 
     def __init__(self) -> None:
         """Initialize a configuration for SystemLink using API key-based
@@ -25,15 +28,16 @@ class JupyterHttpConfiguration(core.HttpConfiguration):
         http_uri = os.environ[self._HTTP_URI_ENV_VAR]
         api_key = os.environ[self._HTTP_API_KEY_ENV_VAR]
 
-        # SystemLink Server 26Q3 restricts notebook executions (by default). Access to the `HttpConfigurations` folder is
-        # no longer granted for notebooks running under the JupyterHub/NotebookExecution services. Since config files under
-        # `HttpConfigurations` are no longer readable, creating client objects from this library, from SLS notebooks,
-        # will default to using this `JupyterHttpConfiguration`. However, this does not set a `cert_path`, so HTTPS
-        # requests will fail.
-        # When running SystemLink Server notebooks (Windows), we will therefore pass the Web Server CA certificate's path as the
-        # `cert_path`, if the file exists. This will allow clients created with the default configuration to use this CA certificate.
-        # If the file does not exist (when the Web Server is configured in HTTP mode), do not pass it; an invalid `cert_path`
-        # will lead to errors.
+        # SystemLink Server 26Q3 restricts notebook executions (by default). Access to the `HttpConfigurations` folder
+        # is no longer granted for notebooks running under the JupyterHub/NotebookExecution services. Since config
+        # files under `HttpConfigurations` are no longer readable, creating client objects from this library, from SLS
+        # notebooks, will default to using this `JupyterHttpConfiguration`. However, this does not set a `cert_path`,
+        # so HTTPS requests will fail.
+        # When running SystemLink Server notebooks (Windows), we will therefore pass the Web Server CA certificate's
+        # path as the `cert_path`, if the file exists. This will allow clients created with the default configuration
+        # to use this CA certificate.
+        # If the file does not exist (when the Web Server is configured in HTTP mode), do not pass it; an invalid
+        # `cert_path` will lead to errors.
         if sys.platform.startswith("win") and os.path.exists(
             self._SYSTEMLINK_SERVER_CERT_PATH
         ):
