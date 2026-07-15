7f393b7403e2a99ec9e9c0b4dd5c24c8a29db415
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2026-07-15 19:26:40 +0300

    Pass hardcoded cert_path on Windows, so CA file is properly located when using the clients library from SLS notebooks

--- nisystemlink/clients/core/_jupyter_http_configuration.py
+++ nisystemlink/clients/core/_jupyter_http_configuration.py
@@ -3,6 +3,7 @@
 """Implementation of JupyterHttpConfiguration."""
 
 import os
+import sys
 
 from nisystemlink.clients import core
 
@@ -12,6 +13,7 @@ class JupyterHttpConfiguration(core.HttpConfiguration):
 
     _HTTP_URI_ENV_VAR = "SYSTEMLINK_HTTP_URI"
     _HTTP_API_KEY_ENV_VAR = "SYSTEMLINK_API_KEY"
+    _SYSTEMLINK_SERVER_CERT_PATH = r"C:\ProgramData\National Instruments\Skyline\Certificates\http-server\http-server.cer"
 
     def __init__(self) -> None:
         """Initialize a configuration for SystemLink using API key-based
@@ -20,6 +22,12 @@ class JupyterHttpConfiguration(core.HttpConfiguration):
         Raises:
             KeyError: if the expected environment variables are not set.
         """
-        super().__init__(
-            os.environ[self._HTTP_URI_ENV_VAR], os.environ[self._HTTP_API_KEY_ENV_VAR]
-        )
+        http_uri = os.environ[self._HTTP_URI_ENV_VAR]
+        api_key = os.environ[self._HTTP_API_KEY_ENV_VAR]
+
+        if sys.platform.startswith("win"):
+            super().__init__(
+                http_uri, api_key, cert_path=self._SYSTEMLINK_SERVER_CERT_PATH
+            )
+        else:
+            super().__init__(http_uri, api_key)
